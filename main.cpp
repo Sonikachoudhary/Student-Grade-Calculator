0322dbfa57203298c5ab877704c39b98f28015e0
Sonika Choudhary <sonikachoudhary2006@gmail.com>
2026-09-16 16:56:03 +0530

    Added grade calculation

--- main.cpp
+++ main.cpp
@@ -20,6 +20,20 @@ int main() {
   float percentage = (total/500.0)*100;
   
     cout<< "Percentage:" << percentage <<"%" <<endl;
+
+  char grade;
+  if (percentage>=90)
+    grade = 'A';
+  else if (percentage>=80)
+    grade = 'B';
+      else if (percentage>=70)
+      grade = 'C';
+        else if (percentage>=60)
+          grade = 'D';
+  else
+          grade = 'F';
+  cout<<"Grade:"<<grade<<endl;
+        
   
   return 0;
 }
